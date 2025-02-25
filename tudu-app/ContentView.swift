112063bdd459b1220c96c5b32bf8e309e122c792
Do Tran <dotran150797@gmail.com>
2025-02-25 13:43:18 +0700

    Initial Commit

---
+++ tudu-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tudu-app
+//
+//  Created by Do Tran on 25/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tudu-app/tudu_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tudu_appApp.swift
+//  tudu-app
+//
+//  Created by Do Tran on 25/2/25.
+//
+
+import SwiftUI
+
+@main
+struct tudu_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
